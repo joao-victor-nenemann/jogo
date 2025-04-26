eee3e44ee792bbdbe3cdabd98e403ba2c5ff1066
joao victor <nenemannprogrsming@gmail.com>
2025-04-26 16:58:11 -0300

    versão 1

---
+++ main.js
@@ -0,0 +1,4 @@
+  const toggle = document.querySelector('.nav-toggle');
+  const menu  = document.querySelector('nav ul');
+  toggle.addEventListener('click', () => menu.classList.toggle('open'));
+
